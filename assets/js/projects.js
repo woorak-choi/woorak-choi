7a9e70d36d526baea2a5b384274d8f34acd72729
woorak-choi <83800496+woorak-choi@users.noreply.github.com>
2024-05-10 10:20:10 +0900

    Add files via upload

---
+++ assets/js/projects.js
@@ -0,0 +1,81 @@
+$(document).ready(() => {
+    render_projects('featured');
+})
+
+
+let render_projects = (slug) => {
+    let projects_area = $('.projects-wrapper');
+
+    $('.white-button').removeClass('white-button-hover');
+    $(`#${slug}`).addClass('white-button-hover');
+
+    let projects_obj = [
+        {
+            image: 'assets/images/mentors.jpg',
+            link: 'https://github.com/abhn/Mporter',
+            title: 'Mporter',
+            demo: 'https://mporter.co',
+            technologies: ['Flask', 'Celery', 'Python'],
+            description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
+            categories: ['featured', 'webdev']
+        },
+        {
+            image: 'assets/images/mobile-landscape.jpg',
+            link: 'https://github.com/abhn/Wall-E',
+            title: 'Wall-E',
+            demo: 'http://wall-e-jekyll.github.io/',
+            technologies: ['Semantic UI', 'Jekyll'],
+            description: "A modern Jekyll theme with grid frontpage, beautiful typography, mobile responsive, made with Semantic UI.",
+            categories: ['featured', 'webdev']
+        },
+    ]
+
+    let projects = [];
+    if(slug == 'all') {
+        projects = projects_obj.map(project_mapper);
+    } 
+    else {
+        projects = projects_obj.filter(project => project.categories.includes(slug)).map(project_mapper);
+    }
+    projects_area.hide().html(projects).fadeIn();
+}
+
+let project_mapper = project => {
+    return `
+        <div class="wrapper">
+                
+            <div class="card radius shadowDepth1">
+
+                ${project.image ? 
+                    `<div class="card__image border-tlr-radius">
+                        <a href="${project.link}">
+                            <img src="${project.image}" alt="image" id="project-image" class="border-tlr-radius">
+                        </a>
+                    </div>`           
+                : ''}
+
+        
+                <div class="card__content card__padding">
+        
+                    <article class="card__article">
+                        <h2><a href="${project.link}">${project.title}</a></h2>
+        
+                        <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}">Demo</a>` : ''}</p>
+                    </article>
+
+                                
+                    <div class="card__meta">
+                        ${project.technologies.map(tech =>
+                            `<span class="project-technology paragraph-text-normal">${tech}</span>`
+                        ).join('')}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `
+}
+
+let selected = (slug) => {
+    render_projects(slug);
+}
